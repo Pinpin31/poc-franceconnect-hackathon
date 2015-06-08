a3b3e141496f8aa712d4aa0ac38f6184c0564609
sperrinel <syperrinel@gmail.com>
2015-06-08 22:38:51 +0200

    Ajout tuiles en page d'accueil

--- poc-client/js/app-navigation.js
+++ poc-client/js/app-navigation.js
@@ -1,6 +1,6 @@
 angular.module("appNavigationModule", [])
-    .controller('appNavigationController', ['$rootScope', '$scope', '$mdSidenav', '$mdBottomSheet', '$q',
-        function ($rootScope, $scope, $mdSidenav, $mdBottomSheet, $q) {
+    .controller('appNavigationController', ['$rootScope', '$scope', '$mdSidenav', '$mdBottomSheet', '$q', '$location',
+        function ($rootScope, $scope, $mdSidenav, $mdBottomSheet, $q, $location) {
 
             $scope.toggleList = function () {
                 var pending = $mdBottomSheet.hide() || $q.when(true);
@@ -8,6 +8,14 @@ angular.module("appNavigationModule", [])
                 pending.then(function () {
                     $mdSidenav('left').toggle();
                 });
+            };
+            
+            $scope.gotoStart = function(){
+              $location.path('/start');
+            };
+            
+            $scope.gotoApiculteurInfos = function(){
+              $location.path('/apiculteurInfos');
             };
 
         }]);
