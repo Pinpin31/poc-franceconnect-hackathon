8665ed573d5e9294496ab4675034062e51311c08
bper1 <b.perin@gmail.com>
2015-06-18 15:51:49 +0200

    Amélioration de la carte des ruchers

--- poc-api-ruchers-client/js/dashboard.js
+++ poc-api-ruchers-client/js/dashboard.js
@@ -16,10 +16,10 @@
                 };
                 
                 $scope.bar  = {
-                    labels: ["2009", "2010", "2011", "2012", "2013", "2014", "2015"],
-                    series: ['Nombre de ruches'],
+                    labels: ["Aide à la transhumance", "Aide aux ruchers école", "Aide à la lutte contre la varroose", "Aide au maintien du cheptel apicole"],
+                    series: ['Aide'],
                     data: [
-                        [10, 24, 32, 48, 26, 28, 31]
+                        [100, 0, 250, 400]
                     ],
                     onClick: function (points, evt) {
                         console.log(points, evt);
