d73e1f24cfa4004a2c774603774d8f1e80df3131
bper1 <b.perin@gmail.com>
2015-06-19 12:23:51 +0200

    ajout de la declaration

--- poc-api-ruchers-client/js/declarer.js
+++ poc-api-ruchers-client/js/declarer.js
@@ -1,7 +1,40 @@
 angular.module("declarerModule", [])
-    .controller("declarerController", ["$scope", "$rootScope",
-        function($scope, $rootScope) {
+    .controller("declarerController", ["$scope", "$rootScope", "$mdDialog", 
+        function($scope, $rootScope, $mdDialog) {
+            $scope.alert = '';
 
-            $scope.nbRucher = $rootScope.apiculteurs[0].ruchers.lentgh;
+            $scope.nbRucher = $rootScope.apiculteurs[0].ruchers.length;
+            
+            $scope.validerDeclaration = function(ev) {
+                    console.log("Validation de la déclaration");
+                    
+                    $mdDialog.show(
+                    $mdDialog.alert()
+                        .parent(angular.element(document.body))
+                        .title('Déclaration validée')
+                        .content('Merci pour votre déclaration, elle est validée.')
+                        .ariaLabel('Alert Dialog Demo')
+                        .ok('Fermer')
+                        .targetEvent(ev)
+                    );
+                        
+
+                }   
+            
+            
+            
+            
         }
     ]);
+    
+    function DialogController($scope, $mdDialog) {
+        $scope.hide = function() {
+            $mdDialog.hide();
+        };
+        $scope.cancel = function() {
+            $mdDialog.cancel();
+        };
+        $scope.answer = function(answer) {
+            $mdDialog.hide(answer);
+        };
+        }
